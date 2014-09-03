61e3f03f58060b52337c05fd58fc1824761a8a7b
Guilherme Berger <guilherme.berger@gmail.com>
2014-09-02 20:25:02 -0400

    comment about linspace

--- A1/src/im_align.m
+++ A1/src/im_align.m
@@ -1,15 +1,26 @@
 function shifted = im_align(a, b, max_shift, shift_pts)
     [width, height] = size(a);
 
-    rangei = [int8(linspace(-width*max_shift, -1, shift_pts)) 0 int8(linspace(1, width*max_shift, shift_pts))];
-    rangej = [int8(linspace(-height*max_shift, -1, shift_pts)) 0 int8(linspace(1, height*max_shift, shift_pts))];
+    % Generate linearly spaced vectors
+    % These will be used as the values for the shifts
+    ivalues = [
+        int8(linspace(-width*max_shift, -1, shift_pts))
+        0
+        int8(linspace(1, width*max_shift, shift_pts))
+    ];
+    
+    jvalues = [
+        int8(linspace(-height*max_shift, -1, shift_pts))
+        0
+        int8(linspace(1, height*max_shift, shift_pts))
+    ];
     
     min = ssd(a, b, max_shift);
     mini = 0;
     minj = 0;
 
-    for i = rangei
-        for j = rangej
+    for i = ivalues
+        for j = jvalues
             temp = ssd(a, circshift(b, [i j]), max_shift);
             if temp < min
                 min = temp;
